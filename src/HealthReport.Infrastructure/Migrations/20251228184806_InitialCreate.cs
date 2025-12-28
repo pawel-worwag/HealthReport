134f55a729724b192ed848de5ae9c417b80f3072
Paweł Worwąg <pawel.worwag@gmail.com>
2025-12-28 20:22:41 +0100

    Initial migratin

---
+++ src/HealthReport.Infrastructure/Migrations/20251228184806_InitialCreate.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthReport.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BloodGlucoseMeasurements",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    PatientId = table.Column<Guid>(type: "uuid", nullable: true),
+                    MeasuredDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    MeasuredTime = table.Column<TimeOnly>(type: "time without time zone", nullable: false),
+                    BGValue = table.Column<int>(type: "integer", nullable: false),
+                    Meal = table.Column<int>(type: "integer", nullable: false),
+                    Note = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: true),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BloodGlucoseMeasurements", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BloodPressureMeasurements",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    PatientId = table.Column<Guid>(type: "uuid", nullable: true),
+                    MeasuredDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    MeasuredTime = table.Column<TimeOnly>(type: "time without time zone", nullable: false),
+                    WeekOfYear = table.Column<int>(type: "integer", nullable: true),
+                    Systolic = table.Column<int>(type: "integer", nullable: false),
+                    Diastolic = table.Column<int>(type: "integer", nullable: false),
+                    Pulse = table.Column<int>(type: "integer", nullable: true),
+                    Note = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: true),
+                    Source = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BloodPressureMeasurements", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WeightMeasurements",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    PatientId = table.Column<Guid>(type: "uuid", nullable: true),
+                    MeasuredDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    MeasuredTime = table.Column<TimeOnly>(type: "time without time zone", nullable: false),
+                    WeightKg = table.Column<decimal>(type: "numeric(8,2)", precision: 8, scale: 2, nullable: false),
+                    WeightChangeKg = table.Column<decimal>(type: "numeric(8,2)", precision: 8, scale: 2, nullable: true),
+                    BMI = table.Column<decimal>(type: "numeric(5,2)", precision: 5, scale: 2, nullable: true),
+                    BodyFatPercentage = table.Column<decimal>(type: "numeric(5,2)", precision: 5, scale: 2, nullable: true),
+                    SkeletalMuscleMassKg = table.Column<decimal>(type: "numeric(8,2)", precision: 8, scale: 2, nullable: true),
+                    BodyWaterPercentage = table.Column<decimal>(type: "numeric(5,2)", precision: 5, scale: 2, nullable: true),
+                    Source = table.Column<int>(type: "integer", nullable: false),
+                    SourceDetails = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: true),
+                    Note = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: true),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeightMeasurements", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BloodGlucoseMeasurements");
+
+            migrationBuilder.DropTable(
+                name: "BloodPressureMeasurements");
+
+            migrationBuilder.DropTable(
+                name: "WeightMeasurements");
+        }
+    }
+}
