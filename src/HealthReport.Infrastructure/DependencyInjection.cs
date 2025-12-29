68e11ed5f69158aed0d37d42ee52dce1e4c929bd
Paweł Worwąg <pawel.worwag@gmail.com>
2025-12-29 16:49:07 +0100

    Infrastructure layer dependency injection

---
+++ src/HealthReport.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,29 @@
+using HealthReport.Application.Interfaces;
+using HealthReport.Infrastructure.Repositories;
+using HealthReport.Infrastructure.TempFileStorage;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HealthReport.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Configure DbContext (PostgreSQL)
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<HealthReportDbContext>(options =>
+            options.UseNpgsql(connectionString));
+        
+        // Register generic repository implementation
+        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+        
+        // Temp file storage configuration and registration
+        services.Configure<FileSystemTempFileStorageOptions>(configuration.GetSection("FileSystemTempFileStorage"));
+        services.AddSingleton<ITempFileStorage, FileSystemTempFileStorage>();
+        
+        return services;
+    }
+
+}
--- src/HealthReport.Web/Program.cs
+++ src/HealthReport.Web/Program.cs
@@ -1,29 +1,19 @@
-using Microsoft.EntityFrameworkCore;
-using HealthReport.Infrastructure;
 using HealthReport.Application.Interfaces;
 using HealthReport.Infrastructure.TempFileStorage;
 using HealthReport.Application.Services.ImportHandlers;
-using HealthReport.Infrastructure.Repositories;
 using HealthReport.Application.Services.Reports;
+using HealthReport.Infrastructure;
 using HealthReport.Web.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configure DbContext (PostgreSQL)
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<HealthReportDbContext>(options =>
-    options.UseNpgsql(connectionString));
-
-// Register generic repository implementation
-builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddInfrastructure(builder.Configuration);
 
 // Blazor Server
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
-// Temp file storage configuration and registration
-builder.Services.Configure<FileSystemTempFileStorageOptions>(builder.Configuration.GetSection("FileSystemTempFileStorage"));
-builder.Services.AddSingleton<ITempFileStorage, FileSystemTempFileStorage>();
+
 
 // Import handlers
 builder.Services.AddScoped<IBloodPressureImportHandler, BloodPressureImportHandler>();
