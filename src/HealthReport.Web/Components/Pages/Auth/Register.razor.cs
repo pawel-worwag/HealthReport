8f96b9a05ad5a54ee6f5ab941a73aba82b242cf7
Paweł Worwąg <pawel.worwag@gmail.com>
2026-01-01 02:27:01 +0100

    Add user registration page

---
+++ src/HealthReport.Web/Components/Pages/Auth/Register.razor.cs
@@ -0,0 +1,50 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using HealthReport.Identity.Application;
+using HealthReport.Identity.Contracts;
+
+namespace HealthReport.Web.Components.Pages.Auth;
+
+public partial class Register(NavigationManager navigation, IRegisterUserHandler registerHandler)
+	: ComponentBase
+{
+
+
+	protected RegisterModel model = new();
+	protected bool isSubmitting;
+	protected string[]? errors;
+
+	protected async Task HandleValidSubmit()
+	{
+		isSubmitting = true;
+		errors = null;
+		var dto = new RegisterUserDto(model.Email ?? string.Empty, model.Password ?? string.Empty);
+		var result = await registerHandler.RegisterAsync(dto);
+		isSubmitting = false;
+
+		if (result.Succeeded)
+		{
+			navigation.NavigateTo("/user/login");
+		}
+		else
+		{
+			errors = result.Errors;
+		}
+	}
+
+	protected class RegisterModel
+	{
+		[Required]
+		[EmailAddress]
+		public string? Email { get; set; }
+
+		[Required]
+		[MinLength(6)]
+		public string? Password { get; set; }
+
+		[Required]
+		[Compare("Password", ErrorMessage = "Passwords do not match")]
+		public string? ConfirmPassword { get; set; }
+	}
+}
