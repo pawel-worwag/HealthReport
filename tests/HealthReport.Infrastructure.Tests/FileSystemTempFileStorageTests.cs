91cebf7ef3e9ce4b1575d8e3a218146a2f96f2fa
Paweł Worwąg <pawel.worwag@gmail.com>
2026-01-06 20:51:27 +0100

    Add unit tests for `FileSystemTempFileStorage::ResolvePath()` and integrate test project into solution

---
+++ tests/HealthReport.Infrastructure.Tests/FileSystemTempFileStorageTests.cs
@@ -0,0 +1,79 @@
+using System.Reflection;
+using HealthReport.Infrastructure.TempFileStorage;
+using Microsoft.Extensions.Options;
+
+namespace HealthReport.Infrastructure.Tests;
+
+public class FileSystemTempFileStorageTests
+{
+    private static (object instance, MethodInfo method) CreateResolver()
+    {
+        var options = Options.Create(new FileSystemTempFileStorageOptions { BasePath = "/tmp" });
+        var storage = new FileSystemTempFileStorage(options);
+        var method = storage.GetType().GetMethod("ResolvePath", BindingFlags.Instance | BindingFlags.NonPublic);
+        return method is null ? throw new InvalidOperationException() : (storage, method);
+    }
+
+    private static string InvokeResolve(object instance, MethodInfo mi, string? id)
+    {
+        var result = mi.Invoke(instance, [id]);
+        return result is null ? throw new InvalidOperationException() : (string)result;
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("   ")]
+    public void ResolvePath_EmptyOrWhitespace_Throws(string id)
+    {
+        var (storage, method) = CreateResolver();
+        var ex = Assert.Throws<TargetInvocationException>(() => method.Invoke(storage, [id]));
+        Assert.IsType<ArgumentException>(ex.InnerException);
+    }
+
+    [Fact]
+    public void ResolvePath_Null_Throws()
+    {
+        var (storage, method) = CreateResolver();
+        var ex = Assert.Throws<TargetInvocationException>(() => method.Invoke(storage, [null]));
+        Assert.IsType<ArgumentException>(ex.InnerException);
+    }
+
+    [Theory]
+    [InlineData("~/ala.txt")]
+    [InlineData("ala/../ola.txt")]
+    [InlineData("/ola.sh")]
+    [InlineData("/etc/passwd")]
+    [InlineData("../passwd")]
+    public void ResolvePath_PathTraversalOrSeparators_Throws(string id)
+    {
+        var (storage, method) = CreateResolver();
+        var ex = Assert.Throws<TargetInvocationException>(() => method.Invoke(storage, [id]));
+        Assert.IsType<ArgumentException>(ex.InnerException);
+    }
+    
+    
+    [Theory]
+    [InlineData("\0" )]
+    [InlineData("name\0bad")]
+    [InlineData("%2e%2e/%2e%2e")]
+    public void ResolvePath_Bad_Name_Throws(string id)
+    {
+        var (storage, method) = CreateResolver();
+        var ex = Assert.Throws<TargetInvocationException>(() => method.Invoke(storage, [id]));
+        Assert.IsType<ArgumentException>(ex.InnerException);
+    }
+
+    [Theory]
+    [InlineData("ala")]
+    [InlineData("absc-1231-dsce-232e3-sdw")]
+    [InlineData("00fdb775-70ce-47d3-8ffe-5060aa53f78d")]
+    [InlineData("ala..txt")]
+    [InlineData(".ala")]
+    [InlineData("..ala")]
+    public void ResolvePath_ValidNames_ReturnsPath(string id)
+    {
+        var (instance, mi) = CreateResolver();
+        var full = InvokeResolve(instance, mi, id);
+        Assert.Equal(Path.GetFullPath(Path.Combine("/tmp", id)), full);
+    }
+}
