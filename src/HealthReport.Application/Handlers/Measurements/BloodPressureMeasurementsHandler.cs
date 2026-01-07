c8afaa6d4d3f1cfb801414d77709cf160d7c4978
Paweł Worwąg <pawel.worwag@gmail.com>
2026-01-07 23:48:28 +0100

    Remove unused imports from BloodPressureMeasurementsHandler

--- src/HealthReport.Application/Handlers/Measurements/BloodPressureMeasurementsHandler.cs
+++ src/HealthReport.Application/Handlers/Measurements/BloodPressureMeasurementsHandler.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using HealthReport.Application.Interfaces;
 using HealthReport.Domain.Entities;
 using HealthReport.Application.Contracts.Measurements;
