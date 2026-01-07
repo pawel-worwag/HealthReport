9a92ae7ae8e348977b617d21780f1d6982d85b52
Paweł Worwąg <pawel.worwag@gmail.com>
2026-01-07 22:59:47 +0100

    Add date range filters to measurement pages (Blood Pressure, Glucose, Weight)

--- src/HealthReport.Web/Components/Pages/Measurements/BloodPressure.razor.cs
+++ src/HealthReport.Web/Components/Pages/Measurements/BloodPressure.razor.cs
@@ -1,7 +1,19 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 
 namespace HealthReport.Web.Components.Pages.Measurements;
 
 public partial class BloodPressure : ComponentBase
 {
+	// Date range filter - inclusive
+	public DateOnly From { get; set; } = DateOnly.FromDateTime(DateTime.UtcNow.AddDays(-30));
+	public DateOnly To { get; set; } = DateOnly.FromDateTime(DateTime.UtcNow);
+
+	// Apply handler invoked by the "Go" button. Implement loading logic here.
+	public async Task Apply()
+	{
+		// TODO: call service / load data based on From/To
+		await Task.CompletedTask;
+		StateHasChanged();
+	}
 }
